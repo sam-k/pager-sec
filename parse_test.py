c08b5c78a0184627fc27a5e76a6db5e04e865105
Sam Kim <samkim1729@gmail.com>
2021-03-01 03:57:34 -0500

    Test script for parsing trends

---
+++ parse_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+DIRNAME = os.path.dirname(__file__)
+RAW_FILE = os.path.join(DIRNAME, "92957 (1).txt")
+
+def main():
+    with open(RAW_FILE) as f:
+        for line in f:
+            
+
+
+
+if __name__ == "__main__":
+    main()
